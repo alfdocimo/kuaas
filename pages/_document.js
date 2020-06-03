d6e1c4e241131e397dcfd082565ac479db085413
Alfredo <alfredo.narvaez@travelport.com>
2020-06-03 10:12:27 +0200

    add ga integration

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang={this.props.lang || "en"}>
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168379962-1"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'UA-168379962-1');
+              </script>
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
