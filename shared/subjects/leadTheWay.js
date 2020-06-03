a248df61bcfd8f11c0efc6c377fc85de8bb32bfc
Alfredo <alfredo.narvaez@travelport.com>
2020-06-03 22:16:49 +0200

    add messages for team player and lead the way

--- shared/subjects/leadTheWay.js
+++ shared/subjects/leadTheWay.js
@@ -1,4 +1,14 @@
 export default [
   (name) => `Thank you for showing the way ${name}!`,
+  (name) => `Thank you for guiding me professionally and personally ${name}!`,
+  (name) =>
+    `Your thoughtfulness and generosity empower me and encourage me to do my best always ${name}!`,
+  (name) =>
+    `You have an amazing ability to bring out the best in all of us ${name}!`,
+  `Thank you for teaching me that there is always scope for improvement ${name}!`,
+  (name) =>
+    `Your leadership has made us more productive and converted all of our skills into strengths ${name}!`,
   (name) => `Thank you for leading with the example ${name}!`,
+  (name) =>
+    `Thank you for transforming our mistakes into valuable lessons ${name}!`,
 ];
--- shared/subjects/teamPlayer.js
+++ shared/subjects/teamPlayer.js
@@ -1,3 +1,16 @@
 export default [
   (name) => `Thank you for being such a great team player ${name}!`,
+  (name) =>
+    `I don’t know how to show my gratitude for helping me with the latest project. Thank you ${name}!`,
+  (name) =>
+    `Your ideas and insights made it much easier to proceed in the right direction and complete the project successfully ${name}.`,
+  (name) =>
+    `Thank you for sticking out for me. I couldn’t appreciate it enough ${name}!`,
+  (name) =>
+    `Thanks for supporting me wholeheartedly ${name}, your continued support is the one thing I can always count on.`,
+  (name) => `Thank you for being such a great team player ${name}!`,
+  (name) =>
+    `Thank you so much for all your dedication towards the work ${name}! Teamwork makes the dream work happen! `,
+  (name) =>
+    `Thank you for always being your best self and contributing to the work ${name}!`,
 ];
